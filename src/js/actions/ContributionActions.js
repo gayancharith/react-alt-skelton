7832fa89862043292e1feaa275292b7d2a74d208
Madhumal <madhumal@thinkcube.com>
2015-09-22 11:59:23 +0530

    added react alt skelton

---
+++ src/js/actions/ContributionActions.js
@@ -0,0 +1,11 @@
+import alt from '../alt';
+
+class ContribActions {
+  constructor() {
+    this.generateActions(
+      'create'
+    )
+  }
+}
+
+module.exports = alt.createActions(ContribActions);
